133e04ac0487c82fa2d0ed592dcfb4b77c8488b4
PrtkPokhrel <pokhrelpratik52@gmail.com>
2024-06-30 08:00:15 +0545

    initial commit

---
+++ TimeComplexity/constantTC.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World";
+    return 0;
+}
+
+
+// In the above code hello world is printed only once so the time complexity is constant. In notation it is:
+// O(1)
---
+++ TimeComplexity/readme.md
@@ -0,0 +1,1 @@
+In programming big oh(O) notation is used to find the worst case time complexity scenario of the algorithm
